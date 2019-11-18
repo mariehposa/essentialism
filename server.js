325174f586c5370cd4c7f5b430463a985390a953
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-11-18 12:20:47 +0000

    create router for users, projects and values

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
-const server = express()
+const server = express();
+const usersRouter = require('./users/usersRouter')
+const projectsRouter = require('./projects/projectsRouter')
+const valuesRouter = require('./values/valuesRouter')
 
 server.use(helmet())
 server.use(cors())
 server.use(express.json())
+
+
 
 server.get('/', (req, res) => {
     res.json({message: 'Its working!'})

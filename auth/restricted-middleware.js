42dbcdccdb10976a4f0df612d9475489270b236e
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-11-18 17:06:37 +0000

    create middleware to validate user

---
+++ auth/restricted-middleware.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+
+function restricted (req, res, next) {
+    const token = req.headers.authorization;
+
+    if (token) {
+        jwt.verify(
+            token,
+            process.env.SECRET,
+            (error, decodedToken) => {
+                if (error) {
+                    res.status(401).json({
+                        message: 'Unathorized User' + error.message
+                    })
+                } else {
+                    req.decodedToken = decodedToken
+                    next()
+                }
+            }
+        )
+    } else {
+        res.status(400).json({
+            message: 'No credentials provided!'
+        })
+    }
+}

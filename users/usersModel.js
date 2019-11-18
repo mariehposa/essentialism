cdbb29fd481fea71f68ff46b3802a909090a05fd
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-11-18 13:46:20 +0000

    create models for users endpoints

---
+++ users/usersModel.js
@@ -0,0 +1,27 @@
+const db = require('../database/dbConfig');
+
+function getUserBy(userValue) {
+    return db('users as u')
+    .where(userValue)
+    .select('username', 'email')
+    .first()
+}
+
+function getUserId(id) {
+    return db('users as u')
+    .where('u.id', id)
+    .select('username', 'email')
+    .first()
+}
+
+function addUser(user) {
+    return db('users as u')
+    .insert(user)
+    .then(([id]) => this.getUserId(id))
+}
+
+module.exports = {
+    getUserBy,
+    getUserId,
+    addUser
+}

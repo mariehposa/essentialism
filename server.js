ee5417ecf3206b0b392658d9ba14cc42018127ad
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-11-18 12:25:58 +0000

    create endpoint for each router

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ server.use(helmet())
 server.use(cors())
 server.use(express.json())
 
-
+server.use('/api/user', usersRouter)
+server.use('/api/project', projectsRouter)
+server.use('/api/value', valuesRouter)
 
 server.get('/', (req, res) => {
     res.json({message: 'Its working!'})

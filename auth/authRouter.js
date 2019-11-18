0f2c737270a8b2e0d1557e0aa49f87642e56e141
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-11-18 14:57:46 +0000

    endpoint to login user created

--- auth/authRouter.js
+++ auth/authRouter.js
@@ -19,5 +19,27 @@ router.post('/register', (req, res) => {
     })
 })
 
+router.post('/login', (req, res) => {
+    let { username, password } = req.body;
+
+    db.getUserBy({ username })
+    .then(user => {
+        if(user && bcrypt.compareSync(password, user.password)) {
+            const token = generateToken(user);
+            res.status(200).json({
+                token: token
+            })
+        } else {
+            res.status(401).json({
+                message: 'Invalid credentials'
+            })
+        }
+    })
+    .catch(error => {
+        res.status(500).json({
+            message: 'Internal server error!' + error.message
+        })
+    })
+})
 
 module.exports = router;

76d84e2e986447fd11d63275cba121b82c0dbbf1
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-11-18 10:39:25 +0000

    configure knex file to development, testing and production

---
+++ knexfile.js
@@ -0,0 +1,37 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      filename: './database/essentialism.db3'
+    },
+    migrations: {
+      directory: './database/migrations'
+    },
+    seeds: {
+      directory: './database/seeds'
+    },
+  },
+
+  testing: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      filename: './database/test.db3'
+    },
+    migrations: {
+      directory: './database/migrations'
+    },
+    seeds: {
+      directory: './database/seeds'
+    },
+  },
+
+  production: {
+   
+  }
+
+};

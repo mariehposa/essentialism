4239b81c55caa74ce130a09274973f644263e5b4
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-11-22 18:57:43 +0000

    Fix typo

--- users/usersModel.js
+++ users/usersModel.js
@@ -1,59 +1,61 @@
-const db = require('../database/dbConfig');
+const db = require("../database/dbConfig");
 
 function getProjectId(user_id, project_id) {
-    let query = db('projects as p')
-        .where('p.user_id', user_id)
+  let query = db("projects as p");
 
-    if (project_id) {
-        query.where('p.id', project_id)
-    }
+  if (user_id) {
+    query.where("p.user_id", user_id);
+  }
 
-    return query.first()
+  if (project_id) {
+    query.where("p.id", project_id);
+  }
+
+  return query.first();
 }
 
 function getUserProjects(user_id) {
-    return db('projects as p')
-        .where('p.user_id', user_id)
+  return db("projects as p").where("p.user_id", user_id);
 }
 
 function addProject(data) {
-    return db('projects')
-        .insert(data, 'id')
-    .then(([id]) => this.getProjectId(id))
+  return db("projects")
+    .insert(data, "id")
+    .then(([id]) => this.getProjectId(undefined, id));
 }
 
 function updateProject(project_id, data) {
-    return db('projects as p')
-        .where('p.id', project_id)
-        .update(data)
-    // .where(([user_id, id]) => this.getProjectId(user_id, id))
+  return db("projects as p")
+    .where("p.id", project_id)
+    .update(data);
+  // .where(([user_id, id]) => this.getProjectId(user_id, id))
 }
 
 function removeProject(user_id, project_id) {
-    return db('projects as p')
-        .where('p.user_id', user_id)
-        .where('p.id', project_id)
-        .del()
+  return db("projects as p")
+    .where("p.user_id", user_id)
+    .where("p.id", project_id)
+    .del();
 }
 
 function userId(id) {
-    return db('users as u')
-        .where('u.id', id)
-        .first()
+  return db("users as u")
+    .where("u.id", id)
+    .first();
 }
 
 function projectId(id) {
-    return db('projects as p')
-        .where('p.id', id)
-        .first()
+  return db("projects as p")
+    .where("p.id", id)
+    .first();
 }
 
 module.exports = {
-    getProjectId,
-    getUserProjects,
-    addProject,
-    updateProject,
-    removeProject,
-    userId,
-    projectId
-}
+  getProjectId,
+  getUserProjects,
+  addProject,
+  updateProject,
+  removeProject,
+  userId,
+  projectId
+};
